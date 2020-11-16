f418b41199dfd8e27425b6cdb57a9952b997e4c3
Hogyny <35634809+hogyny@users.noreply.github.com>
2020-11-16 22:31:58 +0100

    Create sumFirstPos.cpp

---
+++ sumFirstPos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i = 0, sum =0;
+    for (i ; i<=10 ; i++){
+        sum += i;
+        
+    }
+    
+    cout << "Sum = " << sum;
+    return 0;
+}
